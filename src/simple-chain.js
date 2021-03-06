fcc42dd9286b82a718c163d72a3a89c229797390
Sergey Syleimanov <ssuleimanov.94@gmail.com>
2021-03-06 23:57:06 +0600

    feat: add solution simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,21 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
+    arr: [],
+    getLength() {
+        return this.arr.length
+    },
+    addLink(value) {
+        if (value === undefined) {
+            this.arr.push('( )')
+            return chainMaker
+        } else {
+            this.arr.push('( ' + value + ' )')
+            return chainMaker
+        }
+    }
+    ,
+    removeLink(position) {
+        if (position > this.arr.length || !(Number.isInteger(position)) || typeof position !== "number") {
+            this.arr = [];
+            throw new Error();
+        } else {
+            this.arr.splice(Number(position -1), 1);
+            return chainMaker
+        }
+    },
+    reverseChain() {
+        this.arr.reverse();
+        return chainMaker
+    },
+    finishChain() {
+        let result = this.arr.join('~~');
+        this.arr = [];
+        return result
+    },
 
-  },
-  addLink(value) {
-
-  },
-  removeLink(position) {
-
-  },
-  reverseChain() {
-
-  },
-  finishChain() {
-
-  }
 };
 
 module.exports = chainMaker;
