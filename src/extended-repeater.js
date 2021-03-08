f9c2a1777b9b2b21aeea517e74a559a6188b2872
Sergey Syleimanov <ssuleimanov.94@gmail.com>
2021-03-08 17:41:47 +0600

    feat: add solution extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    let repeatTimes = options.repeatTimes,
+        separator = String(options.separator),
+        addition = String(options.addition),
+        additionRepeatTimes = options.additionRepeatTimes,
+        additionSeparator = String(options.additionSeparator),
+        result,
+        resultAddition;
+    if (!options.hasOwnProperty('separator')) {
+        separator = "+";
+    }
+    if (!options.hasOwnProperty('additionSeparator')) {
+        additionSeparator = '|';
+    }
+    if (!options.hasOwnProperty('repeatTimes')) {
+        repeatTimes = 1;
+    }
+    if (!options.hasOwnProperty('additionRepeatTimes')) {
+        additionRepeatTimes = 1;
+    }
+    if (!options.hasOwnProperty('addition')) {
+        addition = null
+    }
+    if (!options.hasOwnProperty('addition')) {
+        result = (str + separator).repeat(repeatTimes);
+        return result.substring(0, result.length - separator.length)
+    }
+    resultAddition = (addition + additionSeparator).repeat(additionRepeatTimes)
+    result = (String(str) + resultAddition.substring(0, resultAddition.length - additionSeparator.length) + separator).repeat(repeatTimes)
+
+    return result.substring(0, result.length - separator.length)
+}
