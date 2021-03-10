64f296b8fcd3059b8cdc6d1a05afcd4e29e327ab
Sergey Syleimanov <ssuleimanov.94@gmail.com>
2021-03-10 22:45:36 +0600

    feat: add solution 75% extended-repeater.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    encrypt(message, key) {
+        if (message === undefined || key === undefined) {
+            throw new Error()
+
+        } else {
+            let arrMes = message.toUpperCase().split(''),
+                arrKey = key.toUpperCase().split(''),
+                result,
+                resultArr = [];
+
+            const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+            for (let i = 0, k = 0; i < arrMes.length; i++) {
+                if (String(arrMes[i]).match(/[A-Z]/)) {
+                    result = (ALPHABET.indexOf(arrMes[i]) + ALPHABET.indexOf(arrKey[k % arrKey.length])) % ALPHABET.length
+                    resultArr.push(ALPHABET[result])
+                    k++
+                } else {
+                    resultArr.push(arrMes[i])
+                }
+            }
+
+            if (VigenereCipheringMachine) {
+                return resultArr.join('')
+            } else {
+                return resultArr.reverse().join('')
+            }
+        }
+
+    }
+
+    decrypt(message, key) {
+        if (message === undefined || key === undefined) {
+            throw new Error()
+        } else {
+            let arrMes = message.toUpperCase().split(''),
+                arrKey = key.toUpperCase().split(''),
+                result,
+                resultArr = [];
+
+
+
+            const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+            for (let i = 0, k = 0; i < arrMes.length; i++) {
+                if (String(arrMes[i]).match(/[A-Z]/)) {
+                    result = (ALPHABET.indexOf(arrMes[i]) + ALPHABET.length - ALPHABET.indexOf(arrKey[k % arrKey.length])) % ALPHABET.length
+                    resultArr.push(ALPHABET[result])
+                    k++
+                } else {
+                    resultArr.push(arrMes[i])
+                }
+            }
+            if (VigenereCipheringMachine) {
+                return resultArr.join('')
+            } else {
+                return resultArr.reverse().join('')
+            }
+        }
+    }
 }
 
 module.exports = VigenereCipheringMachine;
